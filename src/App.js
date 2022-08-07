f596caccefe4972010bcd9db5dd21fc77733a2a1
AlejandroV01 <alexvera0109@gmail.com>
2022-08-07 14:50:13 -0400

    Overall Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react'
+import { useState, useEffect } from 'react';
 import './App.css';
+import Emoji from './emoji.svg'
 
-function App() {
+const App = () => {
+  const paragraph = document.querySelector('.text')
+  const [joke, setJoke] = useState("")
+
+  const getJoke = () => {
+    setTimeout(() => {
+      fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single')
+    .then(response => response.json())
+    .then(data => {
+      
+      setJoke(`${data.joke}`);
+      paragraph.classList.remove('text-out') 
+      
+    })
+    }, 1000)
+    
+    
+  }
+
+  useEffect(() => {
+    fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single')
+    .then(response => response.json())
+    .then(data => {
+      
+      setJoke(`${data.joke}`);
+      paragraph.classList.remove('text-out') 
+
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="container">
+        <img src={Emoji} alt="" className="emoji"/>
+        <p className='text'>{joke}</p>
+        <button onClick={() => {
+          paragraph.classList.add('text-out') 
+          getJoke()
+          
+          }} className="button">Get Joke</button>
+      </div>
+      
     </div>
   );
 }
 
 export default App;
+
